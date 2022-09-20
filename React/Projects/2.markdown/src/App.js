1556b869da0fc0ce719a2d16196867ce6c56b230
duttrohan0302 <duttrohan0302@gmail.com>
2022-09-20 16:25:15 +0530

    Class based eg & Readme

--- React/Projects/2.markdown/src/App.js
+++ React/Projects/2.markdown/src/App.js
@@ -1,20 +1,54 @@
-import React, { useState } from "react";
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
 import ReactMarkdown from "react-markdown";
 
-function App() {
+// function App() {
+//   const [markdown, setMarkdown] = useState("");
 
-  const [markdown, setMarkdown] = useState("")
+//   const changedMarkdown = (e) => {
+//     setMarkdown(e.target.value);
+//   };
+//   return (
+//     <div className="App">
+//       <textarea
+//         name="textarea"
+//         id="textarea"
+//         onChange={changedMarkdown}
+//       ></textarea>
+//       <ReactMarkdown className="preview" children={markdown} />
+//     </div>
+//   );
+// }
 
-  const changedMarkdown = (e) => {
-      setMarkdown(e.target.value)
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      markdown: ""
+    }
+
+    this.changedMarkdown = this.changedMarkdown.bind(this)
   }
-  return (
-    <div className="App">
-      <textarea name="textarea" id="textarea" onChange={changedMarkdown}></textarea>
-      <ReactMarkdown className="preview" children={markdown} />
-    </div>
-  );
+
+  changedMarkdown(e){
+    this.setState({
+      markdown: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <textarea
+          name="textarea"
+          id="textarea"
+          // onChange={(e)=>this.changedMarkdown(e)}
+          onChange={this.changedMarkdown}
+        ></textarea>
+        <ReactMarkdown className="preview" children={this.state.markdown} />
+      </div>
+    );
+  }
 }
 
 export default App;
